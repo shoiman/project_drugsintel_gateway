c3dd0ecfafa5e762171845a5e24e69be0241142a
shoiman <natarud1988@gmail.com>
2022-08-20 17:49:29 +0300

    first commit

---
+++ src/main/java/drugsintel/UserGatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package drugsintel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class UserGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/drugsintel/UserGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package drugsintel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
